222fc1506a69203c1b34cd9c49e5db29a7fd180f
Theo Stötzer <geometheo@yahoo.de>
2020-02-01 17:53:05 +0100

    finalized player movement
    - tile-movement finished
    - walk animation for robot added
    - created extensions script

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extensions {
+
+    public static Vector3 Round(this Vector3 vec) {
+        vec.x = Mathf.Round(vec.x);
+        vec.z = Mathf.Round(vec.z);
+
+        return vec;
+    }
+
+}
