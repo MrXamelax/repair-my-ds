f60a455675b1dc3e4416ca44f93009bfe47e4d8d
Asuriuz <S_Eisenspan@web.de>
2020-02-02 11:48:44 +0100

    Main Menu Added

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    [SerializeField] Animator robotAnim = null;
+
+    public void StartGame() {
+        GetComponent<Animator>().SetTrigger("start");
+    }
+
+    public void SetRobotMoving() {
+        robotAnim.SetBool("isWalking", true);
+    }
+
+    public void LoadGame() {
+        SceneManager.LoadScene("MainGame");
+    }
+}
